4440ec9eb07b0a3cdaa65a78d34e948540e22746
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-07-05 16:07:22 -0500

    who won

--- game/index.js
+++ game/index.js
@@ -1,5 +1,5 @@
 import { Map } from "immutable";
-
+import { winner } from '../index'
 //Action Types
 const MOVE = "move";
 const START = "START";
@@ -12,19 +12,48 @@ export const move = (turn, position) => ({
 });
 
 //reducer
-export default function reducer(state = { turn: "X", board: Map() }, action) {
-  switch (action.type) {
-    case MOVE:
-      if (state.turn === "X") {
-        state.turn = "O";
-      } else {
-        state.turn = "X";
-      }
-      return {
-        turn: state.turn,
-        board: state.board.setIn(action.position, action.turn)
-      };
-    default:
-      return state;
+// export default function reducer(state = { turn: "X", board: Map() }, action) {
+//   switch (action.type) {
+//     case MOVE:
+//       if (state.turn === "X") {
+//         state.turn = "O";
+//       } else {
+//         state.turn = "X";
+//       }
+//       return {
+//         turn: state.turn,
+//         board: state.board.setIn(action.position, action.turn)
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+function turnReducer(turn = "X", action) {
+  if(action.type === MOVE) {
+    if (turn === "X") {
+      turn = "O";
+    } else {
+      turn = "X";
+    }
+  }
+  return turn;
+}
+
+function boardReducer(board = Map(), action) {
+  if(action.type === MOVE) {
+    return board.setIn(action.position, action.turn)
+  }
+  return board
+}
+
+// const initialState = { turn: "X", board: Map()}
+export default function reducer(state={}, action) {
+  const nextBoard = boardReducer(state.board, action);
+  const winnerState = winner(nextBoard);
+  return {
+    board: nextBoard,
+    turn: turnReducer(state.turn, action),
+    winner: winnerState
   }
 }
--- index.js
+++ index.js
@@ -13,6 +13,80 @@ const printBoard = () => {
   }
 };
 
+const streak = (board, ...remainingCoords) => {
+  let numX = 0;
+  let numO = 0;
+  // console.log(remainingCoords);
+  remainingCoords.forEach(coordinate => {
+    // console.log(coordinate, board.getIn(coordinate))
+    if(board.getIn(coordinate) === 'X') {
+      numX++;
+    }
+    else if(board.getIn(coordinate) === 'O') {
+      numO++;
+    }
+  })
+
+  // console.log(`numX: ${numX}, numO: ${numO}`)
+  if(numX === 3) {
+    return 'X'
+  }
+  else if(numO === 3) {
+    return 'O'
+  }
+  else if(numO + numX === 3) {
+    return 'full'
+  }
+  else {
+    return null
+  }
+}
+
+export const winner = (board)  => {
+  // const { board } = game.getState();
+  console.log("board", board)
+
+  let resultArr = []
+
+  resultArr.push(streak(board, [0, 0], [0, 1], [0 ,2])); //row 1
+  resultArr.push(streak(board, [1, 0], [1, 1], [1 ,2])); //row 2
+  resultArr.push(streak(board, [2, 0], [2, 1], [2 ,2])); //row 3
+  resultArr.push(streak(board, [0, 0], [1, 0], [2 ,0])); //col 1
+  resultArr.push(streak(board, [0, 1], [1, 1], [2 ,1])); //col 2
+  resultArr.push(streak(board, [0, 2], [1, 2], [2 ,2])); //col 3
+  resultArr.push(streak(board, [0, 0], [1, 1], [2 ,2])); //dia 1
+  resultArr.push(streak(board, [2, 0], [1, 1], [0 ,2])); //dia 2
+  // console.log('resultArr' , resultArr)
+
+  let fullCount = 0;
+  let resultString = ''
+  resultArr.forEach(result => {
+    // console.log('result', result)
+    if(result === 'O' || result === 'X') {
+      // console.log('inside our conditional')
+      resultString = `${result} won the game!`
+    }
+    else if(result === 'full') {
+      fullCount++;
+    }
+  })
+
+  // console.log('fullcount', fullCount)
+  // console.log('resultString', resultString)
+  if(fullCount === 8) {
+    console.log('draw')
+    return 'draw'
+  }
+  else if(resultString) {
+    console.log(resultString)
+    return resultString
+  }
+  else {
+    console.log('ongoing')
+    return 'ongoing'
+  }
+}
+
 const getInput = player => async () => {
   const { turn } = game.getState();
   if (turn !== player) return;
@@ -31,11 +105,15 @@ const getInput = player => async () => {
 const game = createStore(gameReducer);
 
 // Debug: Print the state
-game.subscribe(() => console.log(game.getState()));
+//game.subscribe(() => console.log(game.getState()));
+// const { board } = game.getState();
 
 game.subscribe(printBoard);
 game.subscribe(getInput("X"));
 game.subscribe(getInput("O"));
+// const {board} = game.getState()
+// console.log(board)
+// game.subscribe(winner(board));
 
 // We dispatch a dummy START action to call all our
 // subscribers the first time.
