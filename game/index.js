e40e5e6478511b6534eacba9dd49017e5d6ddbb3
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-07-05 12:04:55 -0500

    XO Milestone

--- game/index.js
+++ game/index.js
@@ -1,6 +1,30 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+//Action Types
+const MOVE = "move";
+const START = "START";
+
+//Action Creator
+export const move = (turn, position) => ({
+  type: MOVE,
+  turn,
+  position
+});
+
+//reducer
+export default function reducer(state = { turn: "X", board: Map() }, action) {
+  switch (action.type) {
+    case MOVE:
+      if (state.turn === "X") {
+        state.turn = "O";
+      } else {
+        state.turn = "X";
+      }
+      return {
+        turn: state.turn,
+        board: state.board.setIn(action.position, action.turn)
+      };
+    default:
+      return state;
+  }
 }
